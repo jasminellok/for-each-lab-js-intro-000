e9d3cff5bbf01bb4165ca3807ce7b428e939cf63
Jasmine Lok <jasminellok@gmail.com>
2020-04-15 04:47:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 function iterativeLog(array) {
-  array.forEach((element,index,array) => {
-    console.log(`${index}: ${element}`)
-  })
-}
-
-function iterate(callback) {
-  var arrayInt = [1,2,3,4,5]
-  arrayInt.forEach(callback)
-  return arrayInt
-}
-
-function doToArray(array, callback) {
-  array.forEach(callback)
+  
 }
